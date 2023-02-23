7238885e3a83a075a96f89b24910695c64bc9106
ishake fouhal <i.fouhal@hotmail.com>
2023-02-23 22:08:07 +0100

    close 24#, add a title and a desxription to the swagger ui

---
+++ src/OpenApi/OpenApiFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\OpenApi;
+
+use ApiPlatform\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\OpenApi\OpenApi;
+use ApiPlatform\OpenApi\Model;
+
+class OpenApiFactory implements OpenApiFactoryInterface
+{
+    private $decorated;
+
+    public function __construct(OpenApiFactoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        $openApi = $this->decorated->__invoke($context);
+        //Add a custom title and description to the swagger ui 
+        $openApi = $openApi->withInfo((new Model\Info('Bile-Mo Api', 'v1', 'Documentation of a REST API for a B2B company'))->withExtensionProperty('info-key', 'Info value'));
+        $openApi = $openApi->withExtensionProperty('key', 'Custom x-key value');
+        $openApi = $openApi->withExtensionProperty('x-value', 'Custom x-value value');
+
+        return $openApi;
+    }
+}
